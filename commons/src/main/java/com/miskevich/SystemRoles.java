0e3a45d33ec51c03a340922283f7fe2d99f8e69a
Zheka1234 <102152291+Zheka1234@users.noreply.github.com>
2023-07-30 13:32:10 +0300

    add liquibase

---
+++ commons/src/main/java/com/miskevich/SystemRoles.java
@@ -0,0 +1,7 @@
+package com.miskevich;
+
+public enum SystemRoles {
+
+    ROLE_ADMINISTRATOR, ROLE_SALE_USER, ROLE_CUSTOMER_USER, ROLE_SECURE_API_USER
+
+}
---
+++ rest-api/src/main/java/com/miskevich/TaskDevApplication.java
@@ -0,0 +1,14 @@
+package com.miskevich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskDevApplication {
+
+    public static void main( String[] args ){
+
+        SpringApplication.run( TaskDevApplication.class, args);
+
+    }
+}
