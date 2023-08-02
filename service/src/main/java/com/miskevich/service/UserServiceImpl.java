a8b2610330fee46de964f6dd31cc113d3529bc18
Zheka1234 <102152291+Zheka1234@users.noreply.github.com>
2023-08-02 19:07:35 +0300

    anot

--- service/src/main/java/com/miskevich/service/UserServiceImpl.java
+++ service/src/main/java/com/miskevich/service/UserServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -32,6 +33,7 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
+    @Transactional
     public User create(UserRequest request) {
         log.info("Create new user : {}", request.toString());
 
